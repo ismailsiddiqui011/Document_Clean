7bc2e8d18a34093b5a08c7172dd91bb89bc4f67c
Ismail Siddiqui <61552488+ismailsiddiqui011@users.noreply.github.com>
2022-01-28 04:26:41 +0530

    Update app.py

--- app.py
+++ app.py
@@ -47,14 +47,14 @@ try:
       img = img_loader.img_loader(img)
    except:
       st.markdown('Upload a valid image')
-  img = image_spoiler.image_spoiler(img, brightness, noise)
+  img_c = image_spoiler.image_spoiler(img, brightness, noise)
 
   if choice2 == 'Brute Force':
-      img = array_to_img(img)
+      img = array_to_img(img_c)
       pred, _, _ = brute_force.brute_force(img)
       pred = img_to_array(pred)
   else:
-      pred = model.predict(np.expand_dims(img, 0)/255)[0]
+      pred = model.predict(np.expand_dims(img_c, 0)/255)[0]
       pred = np.clip(pred, 0, 1)
 
   st.image([img, pred], caption = ['Input', 'Prediction'], width = 512)
